60b67b4faf7b2a18b6a0d6b3e28e944e867a2167
Big Core <127689088+BigCoreProject@users.noreply.github.com>
2024-05-17 17:53:54 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,20 @@
-import { ThirdwebProvider } from "@thirdweb-dev/react";
+import { ThirdwebProvider, PayEmbed } from "@thirdweb-dev/react";
 import "../styles/globals.css";
+import { Polygon } from "@thirdweb-dev/chains";
+import { Navbar } from "../components/Navbar/Navbar";
 
 // This is the chain your dApp will work on.
-const activeChain = "mumbai";
+const activeChain = Polygon;
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThirdwebProvider
       activeChain={activeChain}
       clientId={process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID}
-    >
+      >
+      
+      {/* Render the navigation menu above each component */}
+      <Navbar />
       <Component {...pageProps} />
     </ThirdwebProvider>
   );
